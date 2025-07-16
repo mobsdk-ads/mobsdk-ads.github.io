91751eb117f136e7eb9fe21e7337ee65ea63d7b9
liuzhao.wei <liuzhao.wei@argonblue.tech>
2025-07-16 19:46:08 +0800

    1. upload maven files

--- config/file_list.sh
+++ config/file_list.sh
@@ -25,7 +25,7 @@ echo "[" > $JSON_FILE
 # 遍历所有文件并写入 JSON
 FIRST=true
 for FILE in $FILES; do
-  if [[ "${FILE##*.}" == "aar" ]] || [[ "${FILE##*.}" == "gradle" ]] ; then
+  if [[ "${FILE##*.}" == "aar" ]] || [[ "${FILE##*.}" == "gradle" ]] || [[ "${FILE##*.}" == "jar" ]] ; then
     if [ "$FIRST" = true ]; then
       FIRST=false
     else
